3e02a205ff35b733532fa22290171fb989aad91b
augusto <augustocmleal@gmail.com>
2017-09-03 19:19:02 -0300

    Implementação de Controller rest

--- crud/src/main/java/br/com/augustoleal/crud/controller/ClienteController.java
+++ crud/src/main/java/br/com/augustoleal/crud/controller/ClienteController.java
@@ -1,15 +1,30 @@
 package br.com.augustoleal.crud.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.augustoleal.crud.model.Cliente;
+import br.com.augustoleal.crud.repository.ClienteRepository;
+
+@RestController
+@RequestMapping("/cliente")
 public class ClienteController {
 
-//	@Autowired
-//	private ClienteRepository repository;
-//	
-//	@RequestMapping("/insertClient")
-//    public String insertClient(Cliente cliente) {
-//        repository.insertCliente(cliente);
-//        return "Success";
-//    }
+	@Autowired
+	private ClienteRepository repository;
+	
+	@PostMapping("/insertClient")
+    public void insertClient(@RequestBody Cliente cliente) {
+        repository.insertCliente(cliente);
+    }
+	
+	@RequestMapping("/getClients")
+    public List<Cliente> getClients() {
+        return repository.getClientes();
+    }
 }
